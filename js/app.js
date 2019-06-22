fe7ac9d485c857861f934fad737299247433b348
Parvathi Pai <parvathirpai@outlook.com>
2019-06-22 12:48:11 -0700

    Removed feed

--- js/app.js
+++ js/app.js
@@ -20,11 +20,7 @@ var allFeeds = [
     }, {
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
-    }, 
-    {
-        name: ' Testing',
-        url: 'https://parvathirpai.github.io/WebTesting-using-Jasmine/js/app.js'
-    }
+    }    
 ];
 
 /* This function starts up our application. The Google Feed
